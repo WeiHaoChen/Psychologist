43cf8c6709a520459f2eddf1b8537daed611eeac
WeiHaoChen <1453216199@qq.com>
2015-12-24 20:17:07 +0800

    navigation

---
+++ Psychologist/HappinessViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by 陈伟浩 on 15/12/20.
+//  Copyright © 2015年 陈伟浩. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.DataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+            
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    var happiness: Int = 100 {
+        //0 = very sad, 100 = ecstatic
+        didSet
+        {
+            happiness = min(max(happiness,0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI()
+    {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smillnessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+}
---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by 陈伟浩 on 15/12/24.
+//  Copyright © 2015年 陈伟浩. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
